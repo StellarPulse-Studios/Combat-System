08dddaca487e1f5a86251ac4c612d05889d9c959
Arkoprabho Das <87149382+ArkoprabhoDas@users.noreply.github.com>
2024-03-03 22:47:33 +0530

    Enemy Modified

---
+++ Assets/Characters/Enemy/Characters/OrcBehaviourTreeCode/test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject weapon;
+    void Equip()
+    {
+        weapon.SetActive(true);
+    }
+    void UnEquip()
+    {
+        weapon.SetActive(false);    
+    }
+
+}
