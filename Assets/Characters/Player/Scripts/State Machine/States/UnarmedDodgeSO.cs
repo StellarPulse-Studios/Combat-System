59255813ba78dd0b9fe7da4cb34bdcf7ecbb9ebf
Suvankar <yougeneral017@gmail.com>
2024-03-10 17:07:04 +0530

    Modified jump to roll snapping

--- Assets/Characters/Player/Scripts/State Machine/States/UnarmedDodgeSO.cs
+++ Assets/Characters/Player/Scripts/State Machine/States/UnarmedDodgeSO.cs
@@ -8,14 +8,24 @@ namespace Player
     public class UnarmedDodgeSO : StateSO
     {
         public bool snapToGround;
+        public float snapSpeed = 1.0f;
+
+        private bool m_IsSnappingCompleted;
+        private Vector3 m_SnapPosition;
 
         public override void OnEnter(Blackboard board)
         {
             if (snapToGround)
-                SnapToGround(board);
-
-            board.animator.applyRootMotion = true;
-            board.animator.SetTrigger("Roll");
+            {
+                m_IsSnappingCompleted = false;
+                m_SnapPosition = GetSnapPosition(board);
+                board.Velocity = Vector3.zero;
+            }
+            else
+            {
+                board.animator.applyRootMotion = true;
+                board.animator.SetTrigger("Roll");
+            }
         }
 
         public override void OnExit(Blackboard board)
@@ -30,17 +40,42 @@ namespace Player
 
         public override void OnUpdate(Blackboard board)
         {
+            if (snapToGround && !m_IsSnappingCompleted)
+                SnapToGround(board);
+        }
 
+        public override void DrawGizmos(Blackboard board)
+        {
+            base.DrawGizmos(board);
+
+            Gizmos.color = new Color(1.0f, 0.0f, 1.0f, 0.5f);
+            Gizmos.DrawSphere(m_SnapPosition, 0.03f);
+
+            Gizmos.color = new Color(1.0f, 0.0f, 1.0f, 1.0f);
+            Gizmos.DrawLine(board.playerTransform.position, m_SnapPosition);
+        }
+
+        private Vector3 GetSnapPosition(Blackboard board)
+        {
+            Vector3 currPos = board.playerTransform.position;
+            Ray ray = new Ray(currPos + Vector3.up * 0.5f, Vector3.down);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, board.groundLayer))
+                currPos.y = hit.point.y;
+
+            return currPos;
         }
 
         private void SnapToGround(Blackboard board)
         {
-            Ray ray = new Ray(board.playerTransform.position + Vector3.up * 0.5f, Vector3.down);
-            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, board.groundLayer))
+            board.Velocity.y = -snapSpeed;
+            board.characterController.Move(board.Velocity * Time.deltaTime);
+
+            if (board.isGrounded)
             {
-                Vector3 pos = board.playerTransform.position;
-                float yOffset = hit.point.y - pos.y;
-                board.characterController.Move(new Vector3(0.0f, yOffset, 0.0f));
+                m_IsSnappingCompleted = true;
+                board.animator.applyRootMotion = true;
+                board.animator.SetTrigger("Roll");
             }
         }
     }
